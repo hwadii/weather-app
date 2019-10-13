b36588f84f1c37c3e55aa0554169af1f65c2c95b
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-10-13 15:21:39 +0200

    Add geolocation

--- src/Weather.js
+++ src/Weather.js
@@ -2,35 +2,59 @@ import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import * as utils from "./utils";
 
+const DEBUG = false;
+
 function Weather() {
   const [weather, setWeather] = useState({});
   const [coords, setCoords] = useState({ lat: null, lon: null });
   const [isLoading, setIsLoading] = useState(true);
 
   const getLocation = () => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(pos => {
-        setCoords({ lat: pos.coords.latitude, lon: pos.coords.longitude });
+    if (localStorage.getItem("lat") && localStorage.getItem("lon")) {
+      setCoords({
+        lat: localStorage.getItem("lat"),
+        lon: localStorage.getItem("lon")
       });
     } else {
-      alert("Geolocation is not available");
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(pos => {
+          setCoords({ lat: pos.coords.latitude, lon: pos.coords.longitude });
+          localStorage.setItem("lat", pos.coords.latitude);
+          localStorage.setItem("lon", pos.coords.longitude);
+        });
+      } else {
+        alert("Geolocation is not available");
+      }
     }
   };
   const fetchData = async () => {
-    const result = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&APPID=9256a715d51522e13cca79f1290ea644`
-    );
-    const data = await result.json();
-    const weatherData = {
-      id: data.id,
-      city: data.name,
-      country: data.sys.country,
-      temp: data.main.temp,
-      details: data.weather[0],
-      wind: data.wind
-    };
-    setWeather(weatherData);
-    setIsLoading(false);
+    if (!DEBUG) {
+      const result = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&APPID=9256a715d51522e13cca79f1290ea644`
+      );
+      const data = await result.json();
+      console.log(data);
+      const weatherData = {
+        city: data.name,
+        country: data.sys.country,
+        temp: data.main.temp,
+        details: data.weather[0],
+        wind: data.wind
+      };
+      document.title = `Weather in ${weatherData.city}, ${weatherData.country}`;
+      setWeather(weatherData);
+      setIsLoading(false);
+    } else {
+      const weatherTest = {
+        city: "Marseille",
+        country: "FR",
+        temp: "280",
+        details: { description: "rain" }
+      };
+      document.title = `Weather in ${weatherTest.city}, ${weatherTest.country}`;
+      setWeather(weatherTest);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -39,18 +63,22 @@ function Weather() {
     } else {
       getLocation();
     }
-  }, [coords]);
+  });
 
   return (
     <div className="Weather">
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <p>Getting weather info...</p>}
       {!isLoading && (
         <div className="Weather-box">
-          <p>
-            {weather.city}, {weather.country}
-          </p>
-          <p>{weather.details && weather.details.description}</p>
-          <p>{weather.temp && utils.kelvinToDegrees(weather.temp)}</p>
+          <div className="Weather-location">
+            <h2 className="Weather-location__size">{weather.city}, {weather.country} 📍</h2>
+          </div>
+          <div className="Weather-temp">
+            <h1 className="Weather-temp__size">{weather.temp && utils.kelvinToDegrees(weather.temp)}</h1>
+          </div>
+          <h2 className="Weather-description">
+            {weather.details && weather.details.description}
+          </h2>
         </div>
       )}
     </div>
